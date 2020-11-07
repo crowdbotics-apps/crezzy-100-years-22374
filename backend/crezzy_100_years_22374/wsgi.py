be0f7a4c498c57d46d8106bfe6e35e337e9d0461
Crowdbotics <team@crowdbotics.com>
2020-11-07 05:48:15 +0000

    Update models

--- backend/crezzy_100_years_22374/wsgi.py
+++ backend/crezzy_100_years_22374/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crezzy_100_years_22374.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "crezzy_100_years_22374.settings")
 
 application = get_wsgi_application()
