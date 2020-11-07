878f17aad10a4da07e472c1fa5f6653b0bd0f21b
Crowdbotics <team@crowdbotics.com>
2020-11-07 05:48:56 +0000

    add package blueprint-react-native-addItem-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'Additem169008', human_name: 'AddItem', access_route: 'Additem169008'},
 { name: 'Maps169004', human_name: 'Maps', access_route: 'Maps169004', icon: 'map'},
 { name: 'UserProfile169000', human_name: 'User Profile', access_route: 'UserProfile169000'},
 { name: 'BlankScreen0168977', human_name: 'Blank Screen 0', access_route: 'BlankScreen0168977'},
